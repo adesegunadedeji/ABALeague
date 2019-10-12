d66e63bd19482992c390725d980f4563ac9024c1
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-10-12 10:38:41 -0400

    Deleted

--- ABA_League_app_api/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  
-  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    
-  validates :email, presence: true,  uniqueness: true, format: { with: EMAIL_FORMAT }
-
-end
